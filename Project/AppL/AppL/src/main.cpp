d36e12e74c226e04b151fe795f82a790399963f7
hexindong <970902486@qq.com>
2017-04-13 19:13:48 +0800

    D04013T1913

--- Project/AppL/AppL/src/main.cpp
+++ Project/AppL/AppL/src/main.cpp
@@ -1,83 +1,172 @@
 #include "cv.h"
 #include "highgui.h"
 #include <iostream>
+#include<io.h>
 
 using namespace std;
 using namespace cv;
-const int LENGTH = 256;
-int brightness_value(const Mat & image, int div, int * MBL, int* MBLl, int* MBLh);
+ 
+float brightness_value(const MatND& hist, int div, int * mbl, int*mbll, int* mblh);
+void getJustCurrentFile(string path, vector<string>& files);
+Mat getHistImg(string name, const MatND& hist);
 int MBL = 0;
 int MBLh = 0;
 int MBLl = 0;
+int DMP = 0;
 
-int main( )
+int main()
 {
-	Mat origin = imread("./data/pic1.jpg");
-	imshow("Origin",origin);
-	Mat gray_img;
-	cvtColor(origin, gray_img, CV_RGB2GRAY);
- 
-	brightness_value(gray_img, 1, &MBL,&MBLl,&MBLh);
-
-	waitKey();
+	vector<string> files;
+	getJustCurrentFile("./data/", files);
+	for (int i = 0; i < files.size(); i++)
+	{
+		    //string fileName ="./data/pic" + format("%d", i) + ".png";   // "./data/pic01.png";   // 
+			//Mat origin = imread(fileName);
+		    Mat origin = imread(files[i]);
+			if (origin.empty())
+			{
+				cout << "Read picture failed!" << endl;
+				waitKey();
+				return -1;
+			}
+			Mat gray_img;
+			cvtColor(origin, gray_img, CV_RGB2GRAY);
+			/***********************************************/
+			const int channels[1] = { 0 };
+			const int histSize[1] = { 256 };
+			float hranges[2] = { 0, 255 };
+			const float* ranges[1] = { hranges };
+			MatND hist,hist2;
+			Mat histMask = imread("./data/ZhistMask.jpg");
+			float a = 0.85;
+			float b = (1 -a); 
+			Mat dst;
+			addWeighted(origin, a, histMask, b, 0.0, dst);		 
+			cvtColor(histMask, histMask, CV_RGB2GRAY);
+			threshold(histMask, histMask, 170, 255, CV_THRESH_BINARY);
+			calcHist(&gray_img, 1, channels, histMask, hist, 1, histSize, ranges);
+			calcHist(&gray_img, 1, channels, Mat(), hist2, 1, histSize, ranges);
+			getHistImg("One",hist);
+			getHistImg("Two",hist2);
+			//waitKey();
+			float perc = brightness_value(hist, 1, &MBL, &MBLl, &MBLh);
+			DMP = MBL - (MBLl + MBLh) / 2;
+			string result ="Delta:"+format("%.2f", perc) + "%";
+			string num = "DMP: " + format("%d", DMP) + " MBL: " + format("%d", MBL);
+			putText(dst, result, Point(20,20), FONT_HERSHEY_SIMPLEX,0.5, Scalar(255, 0, 0),1, 4);
+			putText(dst, num, Point(120,20), FONT_HERSHEY_SIMPLEX,0.5, Scalar(255, 0, 0), 1, 4);
+			imshow("result",dst);
+			/***********************************************/
+			waitKey();
+	}
 	return 0;
 }
-/*********************************** 
-*const Mat & image  灰度图
+
+
+
+Mat getHistImg(string name,const MatND& hist_)
+{
+	double maxVal = 0;
+	double minVal = 0;
+
+	//找到直方图中的最大值和最小值
+	minMaxLoc(hist_, &minVal, &maxVal, 0, 0);
+	int histSize = hist_.rows;
+	Mat histImg(histSize, histSize, CV_8U, Scalar(255));
+	// 设置最大峰值为图像高度的90%
+	int hpt = static_cast<int>(0.9*histSize);
+
+	for (int h = 0; h<histSize; h++)
+	{
+		float binVal = hist_.at<float>(h);
+		int intensity = static_cast<int>(binVal*hpt / maxVal);
+		line(histImg, Point(h, histSize), Point(h, histSize - intensity), Scalar::all(0));
+		imshow(name, histImg);
+	}
+
+	return histImg;
+}
+
+/***********************************
+*const Mat & image
 *
 /***********************************/
-vector<int> gray_distribution(LENGTH,0);
-int brightness_value(const Mat & image,int div,int * mbl,int*mbll, int* mblh  )
+float brightness_value(const MatND& hist, int div, int * mbl, int*mbll, int* mblh)
 {
-	int nr = image.rows;
-	int nc = image.cols;
-	if (image.isContinuous())
+	int numPoint = 0;
+	for (size_t i = 0; i<hist.rows; i++)
 	{
-		nr = 1;
-		nc = nc*image.rows*image.channels();
+		*mbl += hist.at<float>(i) * i;
+		numPoint += hist.at<float>(i);
+	}
+	*mbl /= numPoint;
+
+	if (*mbl > 255)
+	{
+		cout << "MBL is out of range!" << "NUM: " << *mbl << endl;
+		return -1;
 	}
 
-	for (int i = 0; i < nr; i++)
-	{
-			const  uchar* inData = image.ptr<uchar>(i);
-			for (int j = 0; j < nc; j++)
-			{
-						if (*inData >=LENGTH)
-						{
-							cout << "Out of range" << endl;
-							return -1;
-						}
-						gray_distribution[*inData++]++;							 
-		      }
- 
-	}
- 
-	for (size_t i = 0; i<LENGTH; i++)
-	{
-		*mbl += gray_distribution[i] * i;
-	}
-	*mbl /= image.rows*image.cols;
-
- 
 	int temp = 0;
-	for (size_t i = *mbl; i < LENGTH; i++)
+	for (size_t i = *mbl; i <hist.rows; i++)
 	{
-		*mblh += gray_distribution[i] * i;
-		temp += gray_distribution[i];
+		*mblh += hist.at<float>(i)* i;
+		temp += hist.at<float>(i);
 	}
-	*mblh /= temp;
-	
- 
+
+	if (temp == 0)
+	{
+		*mblh = 0;
+	}
+	else
+	{
+		*mblh /= temp;
+	}
+
 	int temp1 = 0;
 	for (size_t i = 0; i < *mbl; i++)
 	{
-		*mbll+= gray_distribution[i] * i;
-		temp1 += gray_distribution[i];
+		*mbll += hist.at<float>(i) * i;
+		temp1 += hist.at<float>(i);
 	}
+	if (temp1 == 0)
+	{
+		*mbll = 0;
+	}
+	else
+	{
 		*mbll /= temp1;
+	}
 
-	return 0;
+	float percent = (float)(*mbl - (*mbll + *mblh) / 2) * 100 / *mbl;
+
+
+	return percent;
 
 }
 
-
+void getJustCurrentFile(string path, vector<string>& files)
+{
+	//文件句柄  
+	long   hFile = 0;
+	//文件信息  
+	struct _finddata_t fileinfo;
+	string p;
+	if ((hFile = _findfirst(p.assign(path).append("\\*").c_str(), &fileinfo)) != -1)
+	{
+		do
+		{
+			if ((fileinfo.attrib &  _A_SUBDIR))
+			{
+				;
+			}
+			else
+			{
+				string temp = path + fileinfo.name;
+				files.push_back(temp);
+				//files.push_back(p.assign(path).append("\\").append(fileinfo.name) );  
+			}
+		} while (_findnext(hFile, &fileinfo) == 0);
+		_findclose(hFile);
+	}
+}
--- Project/AppL/AppL/src/main2.cpp
+++ Project/AppL/AppL/src/main2.cpp
@@ -1,54 +1,106 @@
-#include <opencv2/photo.hpp>  
-#include <opencv2/highgui.hpp>  
-#include <vector>  
-#include <iostream>  
-#include <fstream>  
+#include "cv.h"
+#include "highgui.h"
+#include <iostream>
 
-using namespace cv;
 using namespace std;
+using namespace cv;
+const int LENGTH = 256;
+int brightness_value(const Mat & image, int div, int * MBL, int* MBLl, int* MBLh);
+int MBL = 0;
+int MBLh = 0;
+int MBLl = 0;
 
-void loadExposureSeq(String, vector<Mat>&, vector<float>&);
-
-int main2(int, char**argv)
+int main2()
 {
-	vector<Mat> images;
-	vector<float> times;
-	loadExposureSeq(argv[1], images, times);
-
-	Mat response;
-	Ptr<CalibrateDebevec> calibrate = createCalibrateDebevec();
-	calibrate->process(images, response, times);
-
-	Mat hdr;
-	Ptr<MergeDebevec> merge_debevec = createMergeDebevec();
-	merge_debevec->process(images, hdr, times, response);
-
-	Mat ldr;
-	Ptr<TonemapDurand> tonemap = createTonemapDurand(2.2f);
-	tonemap->process(hdr, ldr);
-
-	Mat fusion;
-	Ptr<MergeMertens> merge_mertens = createMergeMertens();
-	merge_mertens->process(images, fusion);
-
-	imwrite("fusion.png", fusion * 255);
-	imwrite("ldr.png", ldr * 255);
-	imwrite("hdr.hdr", hdr);
-
+	for (int i = 1; i < 18; i++)
+	{
+		string fileName = "./data/pic" + format("%d", i) + ".jpg";
+		Mat origin = imread(fileName);
+		Mat gray_img;
+		cvtColor(origin, gray_img, CV_RGB2GRAY);
+		int perc = brightness_value(gray_img, 1, &MBL, &MBLl, &MBLh);
+		string result = format("%d", perc) + "%";
+		putText(origin, result, Point(40, 40), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 0, 0), 4, 8);
+		imshow("Origin", origin);
+		waitKey();
+	}
 	return 0;
 }
+/***********************************
+*const Mat & image
+*
+/***********************************/
 
-void loadExposureSeq(String path, vector<Mat>& images, vector<float>& times)
+int brightness_value(const Mat & image, int div, int * mbl, int*mbll, int* mblh)
 {
-	path = path + std::string("/");
-	ifstream list_file((path + "list.txt").c_str());
-	string name;
-	float val;
-	while (list_file >> name >> val) {
-		Mat img = imread(path + name);
-		images.push_back(img);
-		times.push_back(1 / val);
+	int nr = image.rows;
+	int nc = image.cols;
+	vector<int> gray_distribution(LENGTH, 0);
+	if (image.isContinuous())
+	{
+		nr = 1;
+		nc = nc*image.rows*image.channels();
 	}
-	list_file.close();
-}
 
+	for (int i = 0; i < nr; i++)
+	{
+		const  uchar* inData = image.ptr<uchar>(i);
+		for (int j = 0; j < nc; j++)
+		{
+			if (*inData >= LENGTH)
+			{
+				cout << "Out of range" << endl;
+				return -1;
+			}
+			gray_distribution[*inData++]++;
+		}
+
+	}
+
+	for (size_t i = 0; i<LENGTH; i++)
+	{
+		*mbl += gray_distribution[i] * i;
+	}
+	*mbl /= image.rows*image.cols;
+	if (*mbl > 255)
+	{
+		cout << "MBL is out of range!" << "NUM: " << *mbl << endl;
+		return -1;
+	}
+
+	int temp = 0;
+	for (size_t i = *mbl; i < LENGTH; i++)
+	{
+		*mblh += gray_distribution[i] * i;
+		temp += gray_distribution[i];
+	}
+	if (temp == 0)
+	{
+		*mblh = 0;
+	}
+	else
+	{
+		*mblh /= temp;
+	}
+
+	int temp1 = 0;
+	for (size_t i = 0; i < *mbl; i++)
+	{
+		*mbll += gray_distribution[i] * i;
+		temp1 += gray_distribution[i];
+	}
+	if (temp1 == 0)
+	{
+		*mbll = 0;
+	}
+	else
+	{
+		*mbll /= temp1;
+	}
+
+	int percent = (*mbl - (*mbll + *mblh) / 2) * 100 / *mbl;
+
+
+	return percent;
+
+}
